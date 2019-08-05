c3117fefea8a7adf720e6c294ac6466a0cb2f05f
Surendra Rajaneni <srajanen@cisco.com>
2019-08-05 17:22:14 +0530

    blog also added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title= models.CharField(max_length=200)
+    pub_date= models.DateField()
+    body= models.CharField(max_length=20000)
+    image= models.ImageField(upload_to='blogimages')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,4 +132,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/mediaurl/'
